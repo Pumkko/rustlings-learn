a1626babe5c79e60eb58156be7d3d9cda3b40d9e
pumkko <c.coque@hotmail.fr>
2024-05-20 09:56:14 -0400

    complete strings

--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -5,11 +5,18 @@
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
+        println!("That is a color word I know!");
+    } else {
+        println!("That is not a color word I know.");
+    }
+
+    // The next line would not compile if is_a_color_word had the following signature
+    // fn is_a_color_word(attempt: &String) -> bool
+    if is_a_color_word("hello") {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,30 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this!
-    ???
+    /*let mut string_input = String::from(input);
+    string_input.push_str(" world!");
+    string_input */
+
+    //format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let cars_offset = input.find("cars").unwrap_or(input.len());
+    let cars_length_offset = cars_offset + "cars".len();
+
+    let mut string_input = String::from(input);
+
+    string_input.replace_range(cars_offset..cars_length_offset, "balloons");
+    string_input
 }
 
 #[cfg(test)]
